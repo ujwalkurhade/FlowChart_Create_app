2aaa2e6775174ffc4a09631a67c6079824df5870
Ujwal <ujwalkurhade05@gmail.com>
2023-10-04 15:36:45 +0530

    first step adding node

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div>
+      <h1 className="bg-red-600">Hellow Ujwal </h1>
+    </div>
+  );
+};
+
+export default App;
